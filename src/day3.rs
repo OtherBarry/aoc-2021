da2a0f0c56b741bf6707c32342979748859a46ee
Charlie Wilson <charlie.wilson4@gmail.com>
2023-01-31 14:38:18 +1100

    Day 3 Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,16 +1,22 @@
 use std::fs;
 
+const LINE_LENGTH: usize = 12; // TODO: get this from input
+
 fn parse_input(path: &str) -> Vec<Vec<u32>> {
     let input = fs::read_to_string(path).expect("Unable to read file");
     let mut result = Vec::new();
     for line in input.lines() {
-        result.push(line.chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<u32>>());
+        result.push(
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<u32>>(),
+        );
     }
     return result;
 }
 
 fn part1(input: &Vec<Vec<u32>>) -> i32 {
-    let mut count = [0; 12]; // TODO: get this from input
+    let mut count = [0; LINE_LENGTH];
     for line in input {
         for (i, digit) in line.iter().enumerate() {
             count[i] += digit;
@@ -24,8 +30,7 @@ fn part1(input: &Vec<Vec<u32>>) -> i32 {
         if *i as f64 > half {
             gamma_string += "1";
             epsilon_string += "0";
-        }
-        else {
+        } else {
             gamma_string += "0";
             epsilon_string += "1";
         }
@@ -37,10 +42,47 @@ fn part1(input: &Vec<Vec<u32>>) -> i32 {
 }
 
 fn part2(input: &Vec<Vec<u32>>) -> i32 {
-    // TODO: Extract gamma and epsilon calculation into function
-    //     Copy input into two filterable vectors
-    //     Filter out values using gamma/epsilon as bit mask
-    return 0;
+    let mut oxygen_generator_vec = input.clone();
+    for i in 0..LINE_LENGTH {
+        let (a, b): (Vec<Vec<u32>>, Vec<Vec<u32>>) =
+            oxygen_generator_vec.into_iter().partition(|x| x[i] == 0);
+        if a.len() > b.len() {
+            oxygen_generator_vec = a;
+        } else {
+            oxygen_generator_vec = b;
+        }
+        if oxygen_generator_vec.len() == 1 {
+            break;
+        }
+    }
+    println!("Oxygen Generator: {:?}", oxygen_generator_vec);
+    let oxygen_generator_string = oxygen_generator_vec[0]
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<String>();
+    let oxygen_generator = i32::from_str_radix(&oxygen_generator_string, 2).unwrap();
+
+    let mut c02_scrubber_vec = input.clone();
+    for i in 0..LINE_LENGTH {
+        let (a, b): (Vec<Vec<u32>>, Vec<Vec<u32>>) =
+            c02_scrubber_vec.into_iter().partition(|x| x[i] == 0);
+        if a.len() <= b.len() {
+            c02_scrubber_vec = a;
+        } else {
+            c02_scrubber_vec = b;
+        }
+        if c02_scrubber_vec.len() == 1 {
+            break;
+        }
+    }
+    println!("CO2 Scrubber: {:?}", c02_scrubber_vec);
+    let c02_scrubber_string = c02_scrubber_vec[0]
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<String>();
+    let c02_scrubber = i32::from_str_radix(&c02_scrubber_string, 2).unwrap();
+
+    return oxygen_generator * c02_scrubber;
 }
 
 pub fn run(path: &str) {
